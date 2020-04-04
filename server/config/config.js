90d8e4ea3e22923cfbf6edfdaebb68b1dcc3343c
Revanth <revanth.boyapally@sojern.com>
2020-04-04 20:03:25 +0530

    changed sequelize config

---
+++ server/config/config.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "me",
+    password: "rb123",
+    database: "business_book",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  staging: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE,
+    host: process.env.RDS_HOSTNAME,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
